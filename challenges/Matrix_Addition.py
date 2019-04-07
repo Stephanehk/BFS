f4e717d1fc1d9bf94ad8e8e1a2c0724626572258
Stephane <2020shatgiskessell@dwight.edu>
2019-04-07 12:49:16 -0400

    Matrix addition

---
+++ challenges/Matrix_Addition.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  5 12:03:16 2019
+
+@author: 2020shatgiskessell
+"""
+def matrid_add (m1, m2):
+    for i in range(len(m1)):
+        for j in range(len(m1[0])):
+            m2[i][j] = m1[i][j] + m2[i][j]
+    return m2
+
+m1 = [[1,2,3], [4,5,6], [7,8,9], [10,11,12]]
+m2 = [[1,2,3], [4,5,6], [7,8,9], [10,11,12]]
+matrid_add (m1, m2)
